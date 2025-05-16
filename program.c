8aded530d54cf7df6554e96099010629a5c3de25
yuta0325 <yuta.watanabe810@keio.jp>
2025-05-16 08:55:41 +0000

    途中だよ

--- program.c
+++ program.c
@@ -6,20 +6,26 @@
 #define BUFSIZE 1024 
 #define MAX_SEQ_NUM 30 
 #define MAX_GENE_NUM 8
-#define back_A 7519429.0
-#define back_C 4637676.0
-#define back_G 4637676.0
-#define back_T 7519429.0
+#define BACK_A 7519429.0
+#define BACK_C 4637676.0
+#define BACK_G 4637676.0
+#define BACK_T 7519429.0
+#define ATGC 4
+#define large 10000
+int B=0,i=0,j=0;
+int hindo[ATGC][15];
+float kakuritu[ATGC][15];
+float ozzu[ATGC][15];
+int property(int w);
+int wariai(int x);
+int ozz(int y,int u);
 
-
-char g_motif[MAX_SEQ_NUM][BUFSIZE]; //転写因子の結合部位配列を保存する配列
+char g_motif[MAX_SEQ_NUM][BUFSIZE]; 
 
 struct promoter{
   char name[BUFSIZE];
   char seq[BUFSIZE];
-}g_pro[MAX_GENE_NUM]; //遺伝子のプロモータ領域を保存する構造体
-
-//グローバル変数はローカル変数と区別するため、全部大文字にするかg_を先頭につけるのが一般的
+}g_pro[MAX_GENE_NUM]; 
 
 int read_multi_seq(char* filename){
   int seq_num = 0;
@@ -28,14 +34,14 @@ int read_multi_seq(char* filename){
 
   if(fp == NULL){
     printf("motif_region_file open error.\n");
-    exit(1); //ファイルが開けなかった場合プログラムを終了
+    exit(1); 
   }
 
-  while(fscanf(fp, "%s", buffer) != EOF){ //プログラムから一行ずつ読み込む
+  while(fscanf(fp, "%s", buffer) != EOF){ 
     if(buffer[strlen(buffer)-1]=='\n'){
-      buffer[strlen(buffer)-1]='\0'; //改行を切り落とす
+      buffer[strlen(buffer)-1]='\0'; 
     }
-    strcpy(g_motif[seq_num],buffer); //結合部位配列を保存
+    strcpy(g_motif[seq_num],buffer); 
     seq_num++;
   }
   return seq_num;
@@ -67,33 +73,59 @@ int read_promoter(char *filename){
 }
 
 int main(int argc, char* argv[]){
-  int seq_num = read_multi_seq(argv[1]); //１番目の引数で指定した転写因子の複数の結合部位配列を読み込む
+  int seq_num = read_multi_seq(argv[1]); 
+  int gene_num = read_promoter(argv[2]);  
 
-  printf("motif region:\n");
-  for(int i = 0; i < seq_num; i++){
-    printf("%s\n",g_motif[i]); //読み込んだ転写因子の結合部位配列を表示
-  }
-  printf("\n");
-
-  int gene_num = read_promoter(argv[2]);  //２番目の引数で指定した遺伝子のプロモータ領域を読み込む
-  
-  printf("promoter_sequence:\n");
-  for(int i = 0; i < gene_num; i++){
-    printf(">%s\n", g_pro[i].name); //読み込んだプロモータ領域を表示
-    printf("%s\n", g_pro[i].seq);
-  }
-  int B=0;
-  int i=0;
-  int j=0;
   while(g_motif[0][i]!='\0')
   {
     B++;
     i++;
   }
-  int hindo[4][B];
-  for(i=0;i<4;i++)
+  property(B);
+  wariai(B);
+  ozz(B,gene_num);
+
+  char random[large];
+  int num_random[large];
+  char randomhairetu[10][large];
+  int number_random[large-500];
+  for(i=0;i<large;i++)
   {
-    for(j=0;j<B;j++)
+    num_random[i]=rand()%10000;
+    if(num_random[i]<3093)
+    {
+      random[i]='A';
+    }
+    else if(num_random[i]<5000)
+    {
+      random[i]='C';
+    }
+    else if(num_random[i]<6907)
+    {
+      random[i]='G';
+    }
+    else if(num_random[i]<10000)
+    {
+      random[i]='T';
+    }
+  }
+  int z=0;
+  for(i=0;i<10;i++)
+  {
+     number_random[i]=rand()%9500;
+    for(j=0;j<large;j++)
+    {
+      z=number_random[i]+j;
+      randomhairetu[i][j]=random[z];
+    }
+  }
+}
+
+int property(int w)
+{
+  for(i=0;i<ATGC;i++)
+  {
+    for(j=0;j<w;j++)
     {
       hindo[i][j]=0;
     }
@@ -101,7 +133,7 @@ int main(int argc, char* argv[]){
   
   for(i=0;i<15;i++)
   {
-    for(j=0;j<B;j++)
+    for(j=0;j<w;j++)
     {
       if(g_motif[i][j]=='A'){
         hindo[0][j]++;
@@ -121,57 +153,64 @@ int main(int argc, char* argv[]){
     }
   }
 
-  for(i=0;i<4;i++)
+  for(i=0;i<ATGC;i++)
   {
-    for(j=0;j<B;j++)
+    for(j=0;j<w;j++)
     {
       printf("%2d,",hindo[i][j]);
       hindo[i][j]+=1;
     }
     printf("\n");
   }
+  return 0;
+}
 
-  float kakuritu[4][B];
+int wariai(int x)
+{
   int sum=hindo[0][0]+hindo[1][0]+hindo[2][0]+hindo[3][0];
   for(i=0;i<4;i++)
   {
-    for(j=0;j<B;j++)
+    for(j=0;j<x;j++)
     {
       kakuritu[i][j]=(float)hindo[i][j]/sum;
     }
   }
   for(i=0;i<4;i++)
   {
-    for(j=0;j<B;j++)
+    for(j=0;j<x;j++)
     {
       printf("%2f,",kakuritu[i][j]);
     }
     printf("\n");
   }
+  return 0;
+}
 
-  float back_sum=back_A+back_C+back_G+back_T;
-  float qA= back_A/back_sum;
-  float qC= back_C/back_sum;
-  float qG= back_G/back_sum;
-  float qT= back_T/back_sum;
-  float q[4]={qA,qC,qG,qT};
-  
-  float ozzu[4][B];
-  for(i=0;i<4;i++)
+int ozz(int y,int u)
+{
+  float back_sum=BACK_A+BACK_C+BACK_G+BACK_T;
+  float qA= BACK_A/back_sum;
+  float qC= BACK_C/back_sum;
+  float qG= BACK_G/back_sum;
+  float qT= BACK_T/back_sum;
+  float q[ATGC]={qA,qC,qG,qT};
+  printf("%f, %f",qA,qC);
+  for(i=0;i<ATGC;i++)
   {
-    for(j=0;j<B;j++)
+    for(j=0;j<y;j++)
     {
       ozzu[i][j]=log(kakuritu[i][j]/q[i]);
     }
   }
-  for(i=0;i<4;i++)
+  for(i=0;i<ATGC;i++)
   {
-    for(j=0;j<B;j++)
+    for(j=0;j<y;j++)
     {
       printf("%10f,",ozzu[i][j]);
     }
     printf("\n");
   }
+
   int genelen=0;
   int l=0;
   while(g_pro[0].seq[l]!='\0')
@@ -179,31 +218,30 @@ int main(int argc, char* argv[]){
     genelen++;
     l++;
   }
-  int m=0;
-  float score=0;
-  float t=0;
-  int h;
-  for(int d=0;d<8;d++)
+  for(int d=0;d<u;d++)
   {
-    while(m<genelen-B+1)
+    float t=0.0,score=0.0;
+    int m=0,h=0;
+    while(m<genelen-y+1)
     {
-      for(i=0;i<B;i++)
+      score=0.0;
+      for(i=0;i<y;i++)
       {
-        if(g_pro[m+i].seq=='A')
+        if(g_pro[d].seq[m+i]=='A')
         {
-          score+=ozzu[0][i];
+          score=score+ozzu[0][i];
         }
-        else if(g_pro[m+i].seq=='C')
+        else if(g_pro[d].seq[m+i]=='C')
         {
-          score+=ozzu[1][i];
+          score=score+ozzu[1][i];
         }
-        else if(g_pro[m+i].seq=='G')
+        else if(g_pro[d].seq[m+i]=='G')
         {
-          score+=ozzu[2][i];
+          score=score+ozzu[2][i];
         }
-        else if(g_pro[m+i].seq=='T')
+        else if(g_pro[d].seq[m+i]=='T')
         {
-          score+=ozzu[3][i];
+          score=score+ozzu[3][i];
         }
       }
       if(t<score)
@@ -213,12 +251,13 @@ int main(int argc, char* argv[]){
       }
       m++;
     }
-    printf("name=%s\n",g_pro[d].name);
-    printf("score=%f\n",score);
-    for(j=h;j<B;j++)
+    printf("%s\n",g_pro[d].name);
+    printf("score: %f\n",t);
+    printf("gene : ");
+    for(j=h;j<h+y;j++)
     {
-      printf("%c",g_pro[j].seq);
+      printf("%c",g_pro[d].seq[j]);
     }
+    printf("\n\n");
   }
-  return 0;
 }
